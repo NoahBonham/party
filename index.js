f3105b4f4a5491c73215294d66f3f50d24565331
Noah Bonham <nowh717@gmail.com>
2024-08-13 09:06:36 -0700

    restart

--- index.js
+++ index.js
@@ -1,109 +1,28 @@
-const APIURL='https://fsa-crud-2aa9294fe819.herokuapp.com/api/2407-FTB-ET-WEB-FT/events';
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2407-FTB-ET-WEB-FT/events`;
 
-async function logAsync (func){
-    const result = await func();
-    console.log(result);
-}
-const state ={
-    events: [],
-    messages: 'This object is not in the database!',
-    isError: false
+const state = {
+  parties: [],
 };
 
-async function getEvent (){
-    const response = await fetch(`${APIURL}`); 
+const partyList = document.querySelector("#partyList");
+const partyForm = document.querySelector("#partyForm");
+partyForm.addEventListener("submit", addParty);
+partyList.addEventListener("click", deleteParty);
+
+
+async function getParties() {
+  try {
+    const response = await fetch(API_URL);
     const json = await response.json();
-
-    if(!json.success){    
-        throw new Error(json.error);  
-    }
-  state.events = [...json.data];
-
-render();  
+    state.parties = json.data;
+    console.log(json.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function addEventToScreen(r){
-const eventElement = document.getElementById('events');
-const elem = document.createElement('div');
-elem.classList.add('event')
-elem.setAttribute('data-id', r.id);
-
-const dateElem = document.createElement('div');
-dateElem.classList.add('date');
-dateElem.append(r.date);
-
-const locationElem = document.createElement('div');
-locationElem.classList.add('location');
-locationElem.append(r.location);
-
-const descriptionElem = document.createElement('div');
-descriptionElem.classList.add('description');
-descriptionElem.append(r.description)
-
-const delButtonElm = document.createElement('button') 
-const buttonText =document.createElement('Delete'); 
-delButtonElm.appendChild(buttonText);
-
-delButtonElem.addEventListener('click', async (done) => {
-    const selectedEvent = done.target.closets('.event');
-    const id = selectedEvent.dataset.id;
-    const result = await deleteEvent(id)
-})
-
-elem.append(nameElem);
-elem.append(dateElem);
-elem.append(locationElem);
-elem.append(descriptionElem);
-elem.append(delButtonElem);
-
-eventElement.append(elem);
+async function render() {
+  await getParties();
+  renderPartyList();
 }
-
-const form = document.getElementById('eventForm');
-form.addEventListener('submit', async(event) =>{
-    event.preventDefault();
-
-    const id=document.getElement('id');
-    const name = document.getElement('name');
-    const location = document.getElement('location');
-    const date = document.getElement('date');
-    const description = document.getElement('description');
-    const cohortId = document.getElement('cohortId');
-
-    const party ={
-        id: id.value,
-        name:name.value,
-        description:description.value,
-        date:date.value,
-        location:location.value,
-        cohortId:cohortId.value
-    };
-    try{
-        const newEvent = await createEvent(party);
-
-        addEventToScreen(newEvent);
-    }catch(err){
-        console.log(err);
-    }
-});
-
-function render(){
-    state.events.forEach((r) => {
-        addEventToScreen(r);
-    });
-}
-async function init(){
-    await getEvent();
-}
-init();
-
-async function deleteEvent(id){
-    try {
-        const response = await fetch(`${APIURL}${id}`, {
-            method: 'delete',
-        })
-    }
-    catch
-{}
-init()
-}
+render();
